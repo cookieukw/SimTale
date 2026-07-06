da32f7a46e65fccf7faeb49a24fa9fe2c2889880
cookieukw <cookieukw@users.noreply.github.com>
2026-07-06 20:37:59 -0300

    feat: add pregnancy support for SimPlayerComponent

--- src/main/java/com/cookieukw/SimTale/core/SimPlayerComponent.java
+++ src/main/java/com/cookieukw/SimTale/core/SimPlayerComponent.java
@@ -1,5 +1,6 @@
 package com.cookieukw.SimTale.core;
 
+import com.cookieukw.SimTale.core.lifecycle.PregnancyComponent;
 import com.hypixel.hytale.component.Component;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import java.util.UUID;
@@ -11,6 +12,7 @@ public class SimPlayerComponent implements Component<EntityStore> {
     
     public UUID playerUuid;
     public Gender gender;
+    public PregnancyComponent pregnancy;
 
     public SimPlayerComponent() {
     }
@@ -26,6 +28,15 @@ public class SimPlayerComponent implements Component<EntityStore> {
 
     @Override
     public SimPlayerComponent clone() {
-        return new SimPlayerComponent(this.playerUuid, this.gender);
+        SimPlayerComponent clone = new SimPlayerComponent(this.playerUuid, this.gender);
+        if (this.pregnancy != null) {
+            clone.pregnancy = new PregnancyComponent();
+            clone.pregnancy.pregnant = this.pregnancy.pregnant;
+            clone.pregnancy.fatherId = this.pregnancy.fatherId;
+            clone.pregnancy.startTick = this.pregnancy.startTick;
+            clone.pregnancy.durationTicks = this.pregnancy.durationTicks;
+            clone.pregnancy.trimester = this.pregnancy.trimester;
+        }
+        return clone;
     }
 }
