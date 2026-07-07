b6920fdaa8ff378d741b0ff447a80eaa26c6c8ea
cookieukw <cookieukw@users.noreply.github.com>
2026-07-06 22:05:25 -0300

    refactor: clean up entity stat imports and simplify type references in PregnancyManager

--- src/main/java/com/cookieukw/SimTale/core/lifecycle/PregnancyManager.java
+++ src/main/java/com/cookieukw/SimTale/core/lifecycle/PregnancyManager.java
@@ -28,6 +28,9 @@ import com.hypixel.hytale.server.core.modules.entity.component.TransformComponen
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.npc.entities.NPCEntity;
 import org.joml.Vector3d;
+import com.hypixel.hytale.server.core.modules.entitystats.EntityStatMap;
+import com.hypixel.hytale.server.core.modules.entitystats.EntityStatValue;
+import com.hypixel.hytale.server.core.modules.entitystats.asset.DefaultEntityStatTypes;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -194,18 +197,18 @@ public class PregnancyManager {
         float mult = 1.0f + (playerComp.pregnancy.trimester * 0.3f);
         
         Store<EntityStore> store = playerRef.getStore();
-        com.hypixel.hytale.server.core.modules.entitystats.EntityStatMap statMap = 
-            store.getComponent(playerRef, com.hypixel.hytale.server.core.modules.entitystats.EntityStatMap.getComponentType());
+        EntityStatMap statMap = 
+            store.getComponent(playerRef, EntityStatMap.getComponentType());
             
         if (statMap != null) {
-            com.hypixel.hytale.server.core.modules.entitystats.EntityStatValue staminaVal = 
-                statMap.get(com.hypixel.hytale.server.core.modules.entitystats.asset.DefaultEntityStatTypes.getStamina());
+            EntityStatValue staminaVal = 
+                statMap.get(DefaultEntityStatTypes.getStamina());
                 
             if (staminaVal != null) {
                 float current = staminaVal.get();
                 float drainAmount = 0.05f * mult; 
                 float newValue = Math.max(0, current - drainAmount);
-                statMap.setStatValue(com.hypixel.hytale.server.core.modules.entitystats.asset.DefaultEntityStatTypes.getStamina(), newValue);
+                statMap.setStatValue(DefaultEntityStatTypes.getStamina(), newValue);
             }
         }
     }
