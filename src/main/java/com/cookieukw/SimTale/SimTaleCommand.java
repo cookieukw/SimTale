0d412e4f6f08b080448cf35cb157c5443ce41b1d
Cookie <65344982+cookieukw@users.noreply.github.com>
2026-08-11 18:29:34 -0300

    feat: implement child collision handling during carry and add debug putdown command

--- src/main/java/com/cookieukw/SimTale/SimTaleCommand.java
+++ src/main/java/com/cookieukw/SimTale/SimTaleCommand.java
@@ -112,6 +112,7 @@ public class SimTaleCommand extends AbstractPlayerCommand {
         this.addSubCommand(new ForceMarrySubCommand());
         this.addSubCommand(new DebugCommands.DebugBedsSubCommand());
         this.addSubCommand(new DebugCommands.GraveyardSubCommand());
+        this.addSubCommand(new DebugCommands.PutDownSubCommand());
         this.addSubCommand(new DebugCommands.DebugChestsSubCommand());
         this.addSubCommand(new DebugCommands.ForgetSubCommand());
         this.addSubCommand(new PregnancySubCommand());
@@ -147,7 +148,7 @@ public class SimTaleCommand extends AbstractPlayerCommand {
     }
 
     private static void sendUsage(CommandContext ctx) {
-        ctx.sendMessage(Message.raw("Usage: /simtale <spawn|interact|tpall|clearall|forcespawn|forcesleep|forcepreg|forcebirth|setstage|marry|debugbeds|pregnancy|debugnear|setmood|search|toggleai|housecheck|chestcheck|forceeat|forcework|forceplant|setgender|camdebug|unstick|npcstate|forcebabyswap|forcekill|aistatus|setprofession|rescan|babystage|forceplacebaby|forceconstruct|graveyard>"));
+        ctx.sendMessage(Message.raw("Usage: /simtale <spawn|interact|tpall|clearall|forcespawn|forcesleep|forcepreg|forcebirth|setstage|marry|debugbeds|pregnancy|debugnear|setmood|search|toggleai|housecheck|chestcheck|forceeat|forcework|forceplant|setgender|camdebug|unstick|npcstate|forcebabyswap|forcekill|aistatus|setprofession|rescan|babystage|forceplacebaby|forceconstruct|graveyard|putdown>"));
     }
 
     /**
